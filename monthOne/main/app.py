80fdf924863f77664648f841fb955c7392eeefaa
otakurux <mamanideivid33@gmail.com>
2024-02-26 10:44:14 -0400

    add

--- monthOne/main/app.py
+++ monthOne/main/app.py
@@ -4,10 +4,10 @@ def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
     try:
         server_address = ('', 8000)
         httpd = server_class(server_address, handler_class)
-        print('Iniciando servidor web en http://localhost:8000/')
+        print('Iniciando servitor web en http://localhost:8000/')
         httpd.serve_forever()
     except KeyboardInterrupt:
-        print('Apagando servidor web')
+        print('Apagando servitor web')
         httpd.socket.close()
 
 if __name__ == "__main__":
