c87afc5186218b5b737a919172b1f1e1ef903331
otakurux <mamanideivid33@gmail.com>
2024-02-28 09:47:13 -0400

    add homework

--- monthOne/weekFour/02soap/soap_client.py
+++ monthOne/weekFour/02soap/soap_client.py
@@ -1,5 +1,12 @@
 from zeep import Client
 
 client = Client('http://localhost:8000')
-result = client.service.greet(name="Deivid")
-print(result)
+
+result_one = client.service.greet(name="Deivid")
+print(result_one)
+
+result_two = client.service.sum(num1=5, num2=3)
+print(result_two)
+
+result_three = client.service.is_palindrome(word="deivied")
+print(result_three)
--- monthOne/weekFour/02soap/soap_server.py
+++ monthOne/weekFour/02soap/soap_server.py
@@ -4,6 +4,13 @@ from pysimplesoap.server import SoapDispatcher, SOAPHandler
 def greet(name):
     return "¡Hello, {}!".format(name)
 
+def sum(num1, num2):
+    return num1 + num2
+    
+def is_palindrome(word):
+    word = word.lower().replace(" ", "")
+    return word == word[::-1]
+
 dispatcher = SoapDispatcher(
     "example-soap-server",
     location="http://localhost:8000/",
@@ -20,7 +27,26 @@ dispatcher.register_function(
     args={"name": str},
 )
 
+dispatcher.register_function(
+    "sum",
+    sum,
+    returns={"sum": int},
+    args={"num1": int,
+          "num2": int
+          },
+)
+
+dispatcher.register_function(
+    "is_palindrome",
+    is_palindrome,
+    returns={"is_palindrome": bool},
+    args={"word": str},
+)
+
 server = HTTPServer(("0.0.0.0", 8000), SOAPHandler)
 server.dispatcher = dispatcher
 print("Server SOAP initiate en http://localhost:8000/")
 server.serve_forever()
+
+
+# PySimpleSOAP
