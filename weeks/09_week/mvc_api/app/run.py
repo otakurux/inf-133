d1d4841a05a64ae108668a3ce71dbae6bf75d278
otakurux <mamanideivid33@gmail.com>
2024-05-15 12:27:56 -0400

    Avance 15/05/2023

--- weeks/09_week/mvc_api/app/run.py
+++ weeks/09_week/mvc_api/app/run.py
@@ -1,23 +1,27 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 from controllers.animal_controller import animal_bp
 from database import db
 
 app = Flask(__name__)
 
-# Configuración de la base de datos
+SWAGGER_URL = "/api/docs"
+API_URL = "/static/swagger.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "Zoológico API"}
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///zoo.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# Inicializa la base de datos
 db.init_app(app)
 
-# Registra el blueprint de animales en la aplicación
 app.register_blueprint(animal_bp, url_prefix="/api")
 
-# Crea las tablas si no existen
 with app.app_context():
     db.create_all()
 
-# Ejecuta la aplicación
 if __name__ == "__main__":
     app.run(debug=True)
